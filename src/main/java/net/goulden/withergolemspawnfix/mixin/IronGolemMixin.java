cedf836f680ff9ab7cb77c9b63b6950b899ce660
Goulden <geroteijon@gmail.com>
2026-02-05 17:29:09 -0300

    1.0: mod ported to 1.21.11

--- src/main/java/net/goulden/withergolemspawnfix/mixin/IronGolemMixin.java
+++ src/main/java/net/goulden/withergolemspawnfix/mixin/IronGolemMixin.java
@@ -1,8 +1,9 @@
 package net.goulden.withergolemspawnfix.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.animal.golem.IronGolem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,14 +16,14 @@ import static net.goulden.withergolemspawnfix.util.GolemSpawnSafetyHelper.handle
 public class IronGolemMixin {
 
     @Inject(
-            method = "hurt",
+            method = "hurtServer",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private void onHurt(ServerLevel server, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         IronGolem golem = (IronGolem) (Object) this;
 
-        if (source.is(DamageTypes.IN_WALL) && !golem.getPersistentData().getBoolean("SpawnChecked")) {
+        if (source.is(DamageTypes.IN_WALL) && !golem.getPersistentData().getBoolean("SpawnChecked").orElse(false)) {
             cir.cancel();
         }
     }
@@ -34,7 +35,7 @@ public class IronGolemMixin {
     private void onFirstTick(CallbackInfo ci) {
         IronGolem golem = (IronGolem) (Object) this;
 
-        if (!golem.getPersistentData().getBoolean("SpawnChecked") && golem.onGround()) {
+        if (!golem.getPersistentData().getBoolean("SpawnChecked").orElse(false) && golem.onGround()) {
             golem.getPersistentData().putBoolean("SpawnChecked", true);
             handleSpawnSuffocation(golem, 0.4);
         }
