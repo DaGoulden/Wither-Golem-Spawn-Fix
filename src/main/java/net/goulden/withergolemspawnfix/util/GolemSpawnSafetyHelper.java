b00f09f73e8c83d957a3b11531e87ae6810692fc
Goulden <geroteijon@gmail.com>
2026-04-29 17:53:47 -0300

    1.1: tried porting (nw)

--- src/main/java/net/goulden/withergolemspawnfix/util/GolemSpawnSafetyHelper.java
+++ src/main/java/net/goulden/withergolemspawnfix/util/GolemSpawnSafetyHelper.java
@@ -1,7 +1,7 @@
 package net.goulden.withergolemspawnfix.util;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.animal.golem.IronGolem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
